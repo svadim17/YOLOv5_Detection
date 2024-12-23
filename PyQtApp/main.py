0bb85a6ed86b90ed8305e8e6dd1a650af7c934fa
svadim17 <vadim.stetsko@yandex.ru>
2024-12-23 09:42:03 +0300

    - Гомельская версия клиента

--- PyQtApp/main.py
+++ PyQtApp/main.py
@@ -32,7 +32,7 @@ class MainWindow(QMainWindow):
         # central_widget = QWidget(self)
         # self.setCentralWidget(central_widget)
 
-        self.setWindowTitle('NN Recognition')
+        self.setWindowTitle('NN Recognition v1.0.0')
         self.config = self.load_config()
         self.map_list = list(self.config['map_list'])
         self.show_img_status = bool(self.config['show_images'])
