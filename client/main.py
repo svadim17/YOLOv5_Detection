d609462922e7596c9c6e10111b013e8e7fcb1578
svadim17 <vadim.stetsko@yandex.ru>
2025-04-21 14:42:05 +0300

    Version 25.17 Обновлен yolov5 (не запускалось из-за неверных путей внутри yolo)

--- client/main.py
+++ client/main.py
@@ -33,7 +33,7 @@ class MainWindow(QMainWindow):
         # central_widget = QWidget(self)
         # self.setCentralWidget(central_widget)
 
-        self.setWindowTitle('NN Recognition v25.16')
+        self.setWindowTitle('NN Recognition v25.17')
         self.config = self.load_config()
         self.server_ip = list(self.config['server_addr'])
         self.grpc_port = self.config['server_port']
